bbd1bc9311bae0ba220a9e8738fefe7717365591
JinDexter <44965499+JinDexter@users.noreply.github.com>
2020-06-04 20:08:43 +0800

    Update 35_搜索插入位置.java

--- 2020_06_04/35_搜索插入位置.java
+++ 2020_06_04/35_搜索插入位置.java
@@ -14,3 +14,24 @@ class Solution {
         return ptr;
     }
 }
+//Solution 2：
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left =0;
+        int right = nums.length - 1;
+        if(right == 0)
+        return 0;
+
+        while(left <= right){
+            int mid = (left + right) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if (nums[mid] < target){
+                left = mid + 1;
+            }
+            else {right = mid -1;}
+        }
+        return left;
+    }
+}
